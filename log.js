7c6e118a98b7fcdcdd497594ac06c41037045740
Alex Kalyvitis <a.kalyvitis@thebeat.co>
2022-05-20 10:57:44 +0200

    Default log level

--- log.js
+++ log.js
@@ -5,7 +5,7 @@ const transform = getTransformStream();
 transform.pipe(pino.destination(1));
 const log = pino({
     name: "probot",
-    level: process.env.LOG_LEVEL
+    level: process.env.LOG_LEVEL || "info"
 }, transform);
 
 module.exports = log;
